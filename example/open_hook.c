d4448cfcd9c14b1fe59a02e90be47dea96a952f5
jsherman212 <justinsherman419@yahoo.com>
2020-11-16 18:00:58 -0500

    xnuspy_ctl C code makefile/compiles

---
+++ example/open_hook.c
@@ -0,0 +1,87 @@
+#include <errno.h>
+#include <limits.h>
+#include <mach/mach.h>
+#include <pthread.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/sysctl.h>
+#include <sys/syscall.h>
+#include <unistd.h>
+
+static long SYS_xnuspy_ctl = 0;
+
+static int _concat_internal(char **dst, const char *src, va_list args){
+    if(!src || !dst)
+        return 0;
+
+    size_t srclen = strlen(src), dstlen = 0;
+
+    if(*dst)
+        dstlen = strlen(*dst);
+
+    /* Back up args before it gets used. Client calls va_end
+     * on the parameter themselves when calling vconcat.
+     */
+    va_list args1;
+    va_copy(args1, args);
+
+    size_t total = srclen + dstlen + vsnprintf(NULL, 0, src, args) + 1;
+
+    char *dst1 = malloc(total);
+
+    if(!(*dst))
+        *dst1 = '\0';
+    else{
+        strncpy(dst1, *dst, dstlen + 1);
+        free(*dst);
+        *dst = NULL;
+    }
+
+    int w = vsnprintf(dst1 + dstlen, total, src, args1);
+
+    va_end(args1);
+
+    *dst = realloc(dst1, strlen(dst1) + 1);
+
+    return w;
+}
+
+static int concat(char **dst, const char *src, ...){
+    va_list args;
+    va_start(args, src);
+
+    int w = _concat_internal(dst, src, args);
+
+    va_end(args);
+
+    return w;
+}
+
+int main(int argc, char **argv){
+    /* before we begin, figure out what system call was patched */
+    size_t oldlen = sizeof(long);
+    int ret = sysctlbyname("kern.xnuspy_ctl_callnum", &SYS_xnuspy_ctl,
+            &oldlen, NULL, 0);
+
+    if(ret == -1){
+        printf("sysctlbyname with kern.xnuspy_ctl_callnum failed: %s\n",
+                strerror(errno));
+        return 1;
+    }
+
+    /* first, was xnuspy_ctl patched correctly? For all my phones, the patched
+     * system call is always number 8. It could be different for you.
+     */
+    ret = syscall(SYS_xnuspy_ctl, 0, 0, 0, 0);
+
+    if(ret != 999){
+        printf("xnuspy_ctl wasn't patched correctly\n");
+        return 1;
+    }
+
+    printf("xnuspy_ctl was patched correctly\n");
+
+    return 0;
+}
---
+++ module/el1/xnuspy_ctl/README.md
@@ -0,0 +1,3 @@
+# xnuspy_ctl
+
+I wrote this in C because it would have been horrible in asm
