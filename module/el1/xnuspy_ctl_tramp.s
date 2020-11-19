74e6b4957e757b954a477624f1972098f7fb9dd1
jsherman212 <justinsherman419@yahoo.com>
2020-11-18 19:19:09 -0500

    Cleaned up xnuspy_ctl_tramp works

--- module/el1/xnuspy_ctl_tramp.s
+++ module/el1/xnuspy_ctl_tramp.s
@@ -23,10 +23,6 @@ _main:
     ldr x19, [x27, XNUSPY_CTL_IS_RX]
     cbnz x19, handoff
 
-    str xzr, [sp, L1_TTE]
-    str xzr, [sp, L2_TTE]
-    str xzr, [sp, L3_PTE]
-
     ldr x19, [x27, XNUSPY_CTL_CODESTART]
     ldr x20, [x27, XNUSPY_CTL_CODESZ]
     ; we don't need to worry about this not being page aligned. Clang always
@@ -52,7 +48,6 @@ pteloop:
     and x22, x22, 0x7
     add x22, x21, x22, lsl 0x3
     ldr x22, [x22]
-    str x22, [sp, L1_TTE]
     and x0, x22, ARM_TTE_TABLE_MASK
     ldr x22, [x27, PHYSTOKV]
     blr x22
@@ -61,7 +56,6 @@ pteloop:
     and x23, x23, 0x7ff
     add x23, x22, x23, lsl 0x3
     ldr x23, [x23]
-    str x23, [sp, L2_TTE]
     and x0, x23, ARM_TTE_TABLE_MASK
     ldr x22, [x27, PHYSTOKV]
     blr x22
@@ -69,118 +63,21 @@ pteloop:
     lsr x23, x19, ARM_TT_L3_SHIFT
     and x23, x23, 0x7ff
     add x23, x22, x23, lsl 0x3
-
     ; X23 == pointer to PTE for this page
     ldr x24, [x23]
-    str x24, [sp, L3_PTE]
-
     ; ~(ARM_PTE_PNXMASK | ARM_PTE_NXMASK)
     mov x25, 0xff9fffffffffffff
-    ; XXX contiguous hint prevents us from executing the code when we've
-    ; turned off XN and PXN??
     and x24, x24, x25
     str x24, [x27, NEW_PTE_SPACE]
-
-    ; save original PTE contents for comparing later
-    mov x26, x23
-    ldr x25, [x26]
-
     mov x0, x23
     bl _kvtophys
     mov x24, x0
-    ; ldr x23, [x27, KVTOPHYS]
-    ; blr x23
-
-    ; str x0, [sp, REAL_KVTOPHYS_PTEPTR]
-    ; mov x0, x26
-    ; bl _kvtophys
-    ; str x0, [sp, MY_KVTOPHYS_PTEPTR]
-
     add x0, x27, NEW_PTE_SPACE
     bl _kvtophys
-
-    ; blr x23
-    ; str x0, [sp, REAL_KVTOPHYS_PTESPACE]
-    ; add x0, x27, NEW_PTE_SPACE
-    ; bl _kvtophys
-    ; str x0, [sp, MY_KVTOPHYS_PTESPACE]
-
-    ; ldr x1, [sp, REAL_KVTOPHYS_PTEPTR]
-    ; ldr x2, [sp, MY_KVTOPHYS_PTEPTR]
-    ; ldr x3, [sp, REAL_KVTOPHYS_PTESPACE]
-    ; ldr x4, [sp, MY_KVTOPHYS_PTESPACE]
-    ; mov x5, 0x4141
-    ; brk 0
-
-    ; X0 == pa of NEW_PTE_SPACE
     mov x1, x24
     mov w2, 0x8
     ldr x23, [x27, BCOPY_PHYS]
     blr x23
-
-    dsb ish
-    isb sy
-    ; ldr x0, [x27, XNUSPY_CTL_CODESTART]
-    mov x0, x19
-    ldr w1, [x27, XNUSPY_CTL_CODESZ]
-    ldr x23, [x27, FLUSH_MMU_TLB_REGION]
-    blr x23
-
-    ; dmb ish
-    ; dsb ish
-    ; dsb sy
-
-    ; tlbi vmalle1is
-    ; dsb ish
-    ; isb sy
-    ; dmb ish
-    ; dsb ish
-    ; isb sy
-
-
-    ; X1 == L1 tte
-    ldr x1, [sp, L1_TTE]
-    ; X2 == L2 tte
-    ldr x2, [sp, L2_TTE]
-    ; X3 == L3 pte
-    ldr x3, [sp, L3_PTE]
-    ; X4 == original PTE contents
-    mov x4, x25
-    ; X5 == (hopefully) modified PTE contents
-    ldr x5, [x26]
-
-    ; dsb sy
-    ; tlbi vmalle1
-    ; dsb sy
-    ; isb
-
-    ; dc cvau, x19
-    ; dsb ish
-    ; ic ivau, x19
-    ; dsb ish
-    ; isb
-
-    ; dsb sy
-    ; tlbi vmalle1
-    ; XXX this below is an undefined kernel instruction
-    ; tlbi vmalls12e1
-    ; dsb sy
-    ; isb
-
-    ; dsb ishst
-    ; tlbi vmalle1is
-    ; dsb ish
-    ; isb
-
-    ; bcopy_phys succeeds on 13.6.1 a11, not 14.x a10
-    ; mov x0, 0x1234
-    ; mov x1, 0x5678
-    ; brk 0
-    ; isb
-    ; dsb sy
-    ; ic iallu
-    ; dsb sy
-    ; isb
 
 nextpage:
     mov w22, 0x1
@@ -188,33 +85,22 @@ nextpage:
     subs xzr, x20, x19
     b.ne pteloop
 
-    ; dsb sy
-    ; isb
-    ; tlbi vmalle1
-    ; dsb sy
-    ; isb
-
-
-    ; ldr x0, [x27, XNUSPY_CTL_CODESTART]
-    ; ldr w1, [x27, XNUSPY_CTL_CODESZ]
-    ; ldr x19, [x27, FLUSH_MMU_TLB_REGION]
-    ; blr x19
-
-    ; tlbi vmalle1
-    ; dsb ish
-    ; isb
+    isb
+    dsb sy
+    tlbi vmalle1
+    dsb sy
+    isb
 
     str x22, [x27, XNUSPY_CTL_IS_RX]
-    ; fall thru
 
 handoff:
     mov x7, x27
-    ; ldp x0, x1, [sp, STACK-0x10]
-    ; ldp x2, x19, [sp, STACK-0x20]
-    ; ldp x20, x21, [sp, STACK-0x30]
-    ; ldp x22, x23, [sp, STACK-0x40]
-    ; ldp x24, x25, [sp, STACK-0x40]
-    ; ldp x26, x27, [sp, STACK-0x50]
+    ldp x0, x1, [sp, STACK-0x10]
+    ldp x2, x19, [sp, STACK-0x20]
+    ldp x20, x21, [sp, STACK-0x30]
+    ldp x22, x23, [sp, STACK-0x40]
+    ldp x24, x25, [sp, STACK-0x40]
+    ldp x26, x27, [sp, STACK-0x50]
     add sp, sp, STACK
     ldr x7, [x7, XNUSPY_CTL_ENTRYPOINT]
     br x7
