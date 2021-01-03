8bd31e33bae9484c02f0aa5de2dfbb9fe6101c63
jsherman212 <justinsherman419@yahoo.com>
2021-01-03 16:40:08 -0500

    el3 --> el1 --> el3 working

--- module/el3/xnuspy_el3.c
+++ module/el3/xnuspy_el3.c
@@ -1,17 +1,38 @@
-/* #include <stdint.h> */
-
 /* ERET to EL1, execute some code, then issue an SMC */
-static void el1_test(void){
+void el1_test_code(void){
+    int a = 3;
+    a--;
+    int b = a + 3;
+    asm volatile("mrs x8, CurrentEL");
+    asm volatile("smc #0");
+    asm volatile("mrs x8, CurrentEL");
+    int c = 4;
+    c++;
+}
 
+__attribute__ ((naked, noreturn)) static void el1_test(void){
+    asm(""
+        "adrp x0, _el1_test_code@PAGE\n"
+        "add x0, x0, _el1_test_code@PAGEOFF\n"
+        "msr elr_el3, x0\n"
+        "mov x0, 0x3c4\n"
+        "msr spsr_el3, x0\n"
+        "mov x12, 0x3333\n"
+        "isb sy\n"
+        "eret\n"
+        );
 }
 
 __attribute__ ((noreturn)) void xnuspy_el3_entry(void *bootargs,
         void *entrypoint){
-    /* asm volatile("mrs x2, CurrentEL"); */
-    /* asm volatile("mrs x3, VBAR_EL3"); */
+    /* modify PC in debugger to get off of this */
+    asm volatile("b .");
 
-    volatile char *a = (volatile char *)0x41414141;
-    *a = 0;
+    el1_test();
+
+    /* volatile char *a = (volatile char *)0x41414141; */
+    /* *a = 0; */
+    asm volatile("mov x0, 0x1111");
     asm volatile("b .");
     __builtin_unreachable();
 }
@@ -21,7 +42,11 @@ __attribute__ ((noreturn)) void xnuspy_el3_entry(void *bootargs,
  * x8 and x2 is zeroed.
  *
  * We issue an smc to elevate to EL3 before stealing away VBAR_EL3, because
- * pongo's exception vector table is still being used. */
+ * pongo's exception vector table is still being used.
+ *
+ * TODO: configure CPTR_EL3 and the fp stuff so CPACR_EL1 accesses are still
+ * trapped
+ */
 __attribute__ ((naked, noreturn)) void xnuspy_el3_entry_tramp(void){
     asm(""
         "smc #0\n"
